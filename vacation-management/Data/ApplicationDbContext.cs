60719e56d861e46a1a5149a3a2ce86d3a139ef93
Keegan <keegan.adams15@gmail.com>
2021-05-13 17:57:01 -0500

    sqlite update

--- vacation-management/Data/ApplicationDbContext.cs
+++ vacation-management/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace vacation_management.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -17,5 +17,18 @@ namespace vacation_management.Data
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
         public DbSet<LeaveHistory> LeaveHistories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=vacation-management.db;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+            modelBuilder.Entity<LeaveType>().ToTable("LeaveTypes");
+            modelBuilder.Entity<LeaveAllocation>().ToTable("LeaveAllocations");
+            modelBuilder.Entity<LeaveHistory>().ToTable("LeaveHistorys");
+        }
     }
 }
